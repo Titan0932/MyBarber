92f1a60587ae39188bbb477c34aaa1e95f4820fa
Titan0932 <anjishpradhananga@outlook.com>
2024-03-02 14:44:10 -0500

    Backend updates + frontend check tests

---
+++ backend/data/data.py
@@ -0,0 +1,12 @@
+
+
+# export barbers = [
+#   {
+#     'id': 'A123',
+#     'name': "Paul",
+#     'lastname': "Walks",
+#     'queue': [
+#     #   {customers}
+#     ]
+#    }
+# ]
--- backend/services/queueHandler.py
+++ backend/services/queueHandler.py
@@ -4,39 +4,31 @@ import certifi
 import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
 import os
+import json
 
 server1 = Flask(__name__)
 
 load_dotenv()
+
 
 # Callback on connection
 def on_connect(client, userdata, flags, rc):
   print(f'Connected (Result: {rc})')
 
   # See: https://docs.solace.com/Open-APIs-Protocols/MQTT/MQTT-Topics.htm
-  client.subscribe('foo')
-  client.subscribe('hello/#')
-  client.subscribe('languages/+')
-  client.subscribe('game/+/move')
-
-  # Examples of publishing messages to different types of subscriptions
-  client.publish('foo', payload='bar')
-  client.publish('foo/foo', payload='will not be seen, no one is subscribed to this topic')
-
-  client.publish('hello', payload='world')
-  client.publish('hello/world/canada/ottawa', payload='# matches any level')
-
-  client.publish('languages/python2', payload='eol')
-  client.publish('languages/python3', payload='valid')
-  client.publish('languages/other/level', payload='will not be seen, + only matches one level')
-
-  client.publish('game/player1/move', payload='Moving to (1, 2)')
-  client.publish('game/player2/move', payload='Moving to (3, 4)')
+  client.subscribe('queue')
+  
 
 
 # Callback when message is received
 def on_message(client, userdata, msg):
   print(f'Message received on topic: {msg.topic}. Message: {msg.payload}')
+  event = json.loads(msg.payload)
+  # find the di
+  responseMsg = json.dumps({"waitTime":10, "queuePos":5})
+  # once added to queue, send event to a topic
+  client.publish('queueManaged', payload= responseMsg)
+#   manage queue
 
 
 # Set the transport for the client as below
