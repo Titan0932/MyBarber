eb45bceec7e140db22c824c74a35d7cf67e45aff
Titan0932 <anjishpradhananga@outlook.com>
2024-03-02 23:49:26 -0500

    Frontend updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,80 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React,{useState, useEffect} from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.css'; // Make sure to create this CSS file
+// import unknownImg from '../public/img/unknown.png'
 import messaging from "./Messaging";
 import Paho from "paho-mqtt";
 
-const App = () => {
-    const [connected, setConnected] = useState(false);
-    const [messages, setMessages] = useState([]);
+const Table = ({rows}) => {
+  const [curUser, setcurUser] = useState({id: 1232, name: "Lucy Ann"})
+  const [barbers, setbarbers] = useState([])
+  const [bookedBarbers, setbookedBarbers] = useState([])
 
-    useEffect(() => {
-        messaging.register(handleMessage);
-    }, []);
+  useEffect(() => {
+    messaging.connectWithPromise().then(response => {
+        console.log("Successfully connected to Solace Cloud.", response);
+        messaging.subscribe("queueUpdate");
+        console.log("USERID: ", curUser.id)
+        curUser.id && messaging.subscribe(`enqueueRequest/${curUser.id}`);
+        // messaging.subscribe("C138/queueUpdate"); // mock customer ID
+    }).catch(error => {
+        console.log("Unable to establish connection with Solace Cloud, see above logs for more details.", error);
+    });
+    messaging.register("queueUpdate",updateBarbers);
+    messaging.register("enqueueRequest",displayMsg);
+    messaging.register("dequeueUpdate",displayMsg);
+  }, []);
 
-    const handleMessage = (message) => {
-        setMessages(prevMessages => [...prevMessages, message.payloadString]);
-    }
 
-    const handleSendClick = () => {
-        let message = new Paho.Message(JSON.stringify(
-            {
-                "customerID": "C138",
-                "fname": "test_fname",
-                "lname": "test_lname",
-                "waitTime": 0,
-                "barberID": "B124"
-            }
-        ));
-        message.destinationName = "queueRequest";
-        messaging.send(message);
-    }
-
-    const handleSendClick2 = () => {
-        let message = new Paho.Message(JSON.stringify({text: "notificationasdasd"}));
-        message.destinationName = "notification";
-        messaging.send(message);
-    }
-
-    const handleConnectClick1 = () => {
-        if (connected) {
-            messaging.disconnect();
-            setConnected(false);
-        } else {
-            messaging.connectWithPromise().then(response => {
-                console.log("Successfully connected to Solace Cloud.", response);
-                messaging.subscribe("notification");
-                messaging.subscribe("C138/queueUpdate"); // mock customer ID
-                setConnected(true);
-            }).catch(error => {
-                console.log("Unable to establish connection with Solace Cloud, see above logs for more details.", error);
-            });
+  const handleBook = (barberID, action) => {
+    let message = new Paho.Message(JSON.stringify(
+        {
+            "customerID": curUser?.id,
+            "barberID": barberID
         }
-    }
-    const handleConnectClick2 = () => {
-       
-            messaging.connectWithPromise().then(response => {
-                console.log("Successfully connected to Solace Cloud.", response);
-                messaging.subscribe("queue");
-            }).catch(error => {
-                console.log("Unable to establish connection with Solace Cloud, see above logs for more details.", error);
-            });
-    }
-
-    return (
-        <div className="App">
-            <div className="buttons">
-                <button onClick={handleConnectClick1}>{connected ? 'Disconnect' : 'Connect'}</button>
-                <button onClick={handleConnectClick2}>{'Connect2'}</button>
-                {connected ? <button onClick={handleSendClick}>Send</button> : <button disabled>Send</button>}
-                <button onClick={handleSendClick2}>Send2</button>
-            </div>
-            <ol>
-                {messages.map((message, index) => {
-                    return <li key={index}>{message}</li>
-                })}
-            </ol>
-        </div>
-    );
+    ));
+    action == "BOOK" ? message.destinationName = "enqueueRequest" : message.destinationName = "dequeueRequest";
+    messaging.send(message);
 }
+
+  const updateBarbers = (newState) => {
+    setbarbers(newState)
+  }
+
+  const displayMsg = (msg) => {
+    alert(msg)
+  }
+
+
+  return (
+    <div className="d-flex justify-content-center">
+      <table className="table styled-table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Barber</th>
+            <th>Available</th>
+            <th>Wait-time</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((item) => (
+            <tr key={item.id}>
+              <td><img src={'../public/img/unknown.png'} /></td>
+              <td>{item.name}</td>
+              <td>{item.available? "YES!" : "No :("}</td>
+              <td>{item.queue.map((user, index)=> user.id == curUser.id && index * 30)} </td>
+              <td><button className="rounded border-1" onClick={() => {handleBook()}}>Book</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+
+const App = () => {
+  const rows = [
+    {
+      img: 'img/unknown.jpg',
+      id: 1232,
+      name: "John Not Doe",
+      queue: [{id: 111, name: "Lucy Ann"}, {id: 1232, name: "Lucy Ann"}],
+      available: true
+    }
+  ]
+  return (
+    <Table rows={rows} />
+  );
+};
 
 export default App;
--- frontend/src/Messaging.js
+++ frontend/src/Messaging.js
@@ -6,8 +6,13 @@ class Messaging extends Paho.Client {
 	constructor() {
 		super(options.invocationContext.host, Number(options.invocationContext.port), options.invocationContext.clientId);
 		this.onMessageArrived = this.handleMessage.bind(this);
-		this.callbacks = [];
+		this.callbacks = {};
 	}
+
+	topicHandlers = {
+    'queueUpdate': this.handleQueueUpdate,
+    // Add more handlers for other topics as needed
+  };
 
 	connectWithPromise() {
 		return new Promise((resolve, reject) => {
@@ -25,15 +30,27 @@ class Messaging extends Paho.Client {
 		// Add auto connect logic with backoff here if you want to automatically reconnect
 	}
 
-	register(callback) {
-		this.callbacks.push(callback);
+	register(topic, callback) {
+		this.callbacks[topic]= callback;
 	}
 	
 	// called when a message arrives
+
 	handleMessage(message) {
-		console.log("Received message", message.payloadString);
-		this.callbacks.forEach(callback => callback(message));
-	}
+    console.log("Received message", message.payloadString);
+    const topic = message.destinationName;
+
+    if (this.callbacks.topic == "queueUpdate") {
+			let respObj = JSON.parse(message)
+      this.callbacks["queueUpdate"](respObj);
+    } else if(message.contains('enqueueRequest')){
+      this.callbacks["enqueueRequest"](message);
+		}else if(message.contains('dequeueRequest')){
+      this.callbacks["dequeueRequest"](message);
+		}else{
+      console.log(`No handler for topic ${topic}`);
+    }
+  }
 }
 
 const messaging = new Messaging();
